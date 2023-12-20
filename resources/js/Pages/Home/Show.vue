25648f8abd093605d2f6257ac8db2561d2718c20
Bernard Nandwa <bernard.nandwa@un.org>
2023-12-20 23:59:58 +0000

    refactor: use controller to manage the homepage

---
+++ resources/js/Pages/Home/Show.vue
@@ -0,0 +1,16 @@
+<script setup>
+// import Layout from './Layout'
+// import { Head } from '@inertiajs/vue3'
+
+defineProps({ user: Object })
+</script>
+
+<template>
+<!--    <Layout>-->
+    <div>
+        <Head title="Welcome" />
+        <h1>Welcome</h1>
+        <p>App goes here!</p>
+    </div>
+<!--    </Layout>-->
+</template>
